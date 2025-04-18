f3d2787d754b57f3cdcdfbaeb475717cb0496b78
bouzagui <badrbouzagui@gmail.com>
2025-04-18 20:52:50 +0100

    test

--- Frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_CLERK_PUBLISHABLE_KEY)
-  }
-})
